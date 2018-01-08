46c6487b7396457005e15c3c9ccb00d0f622b560
lin1heart <lin1heart@gmail.com>
2018-01-08 19:30:01 +0800

    finish

--- scrapydown/pipelines.py
+++ scrapydown/pipelines.py
@@ -7,19 +7,33 @@
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from scrapy import Request
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 import scrapydown.spiders.scrapydowm
 
 class ScrapydownPipeline(ImagesPipeline):
-    def file_path(self, request, response=None, info=None):
-        image_guid = request.url.split('uploads')[-1].replace('/', '')
-        aa = scrapydown.spiders.scrapydowm.title.split(u'第')[0].replace(u'，', '')
-        print aa
-        return aa+'/%s' % (image_guid)
 
     def get_media_requests(self, item, info):
         for image_url in item['url']:
-            yield Request(image_url)
-
+            yield Request(url=image_url, meta={'item': item, 'img_url':image_url})
+    
+    def file_path(self, request, response=None, info=None):
+        item = request.meta['item']
+        url = request.meta['img_url']
+        aa = item['title']
+        name = ''
+        for check in aa:
+            if check.find(url) !=-1:
+                name = check.replace(url, '')
+                break
+            else:
+                name = 'error'
+        name = self.format_str(name)
+#        print 'name is ', name
+        image_guid = request.url.split('uploads')[-1].replace('/', '')
+        print image_guid
+        return name+'/%s' % (image_guid)
+    
     def item_completed(self, results, item, info):
         # print 'item is !!!!!', item
         # print 'results is !!!!!', results
@@ -27,3 +41,20 @@ class ScrapydownPipeline(ImagesPipeline):
         if not image_paths:
             raise DropItem('图片未下载好 %s'%image_paths)
         return item
+
+    def is_chinese(self, uchar):
+#    """判断一个unicode是否是汉字"""
+        if uchar >= u'\u4e00' and uchar <= u'\u9fa5':
+            return True
+        else:
+            return False
+        
+    def format_str(self, content):
+        if content == 'error':
+            return 'error'
+        else:
+            content_str = ''
+            for i in content:
+                if self.is_chinese(i):
+                    content_str = content_str+i
+            return content_str
--- scrapydown/spiders/scrapydowm.py
+++ scrapydown/spiders/scrapydowm.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import string
 from scrapy.selector import Selector
 from scrapydown.items import ScrapydownItem
 title = ''
@@ -17,15 +18,15 @@ class ScrapydowmSpider(scrapy.Spider):
 
     def parse(self, response):
         global detil_title
+        global title
         sear = Selector(response)
         total_ui = sear.xpath('//ul[@class="wp-list clearfix"]')
-        # print total_ui
         for each in total_ui:
             detil_url = each.xpath('//li/div/h3/a/@href').extract()
             detil_title = each.xpath('//li/div/h3/a/b/text()').extract()
         for a in detil_url:
             yield scrapy.http.Request(a, callback=self.detil)
-
+            
         next_url = sear.xpath('//*[@id="wp_page_numbers"]/ul/li')[-2].xpath('a/@href').extract()
         if next_url:
             next = next_url[0]
@@ -33,12 +34,32 @@ class ScrapydowmSpider(scrapy.Spider):
             yield scrapy.http.Request('http://www.meizitu.com/a/' + next, callback=self.parse)
 
     def detil(self, response):
-        global title
+#        global title
         sel = Selector(response)
         image_url = sel.xpath('//*[@id="picture"]/p/img/@src').extract()
         title = sel.xpath('//*[@id="picture"]/p/img/@alt')[0].extract()
+        if len(title) ==0:
+            title = 'error'
+        else:
+            title = title.split(u'第')[0].replace(u'，', '')
+        url_title = []
+        for each in image_url:
+            url_title.append(each + title)
         item = ScrapydownItem()
         item['url'] = image_url
-        item['title'] = detil_title
-        print 'title is ', title
+        item['title'] = url_title
         yield item
+        
+    def is_chinese(self, uchar):
+#    """判断一个unicode是否是汉字"""
+        if uchar >= u'\u4e00' and uchar <= u'\u9fa5':
+            return True
+        else:
+            return False
+        
+    def format_str(self, content):
+        content_str = ''
+        for i in content:
+            if self.is_chinese(i):
+                content_str = content_str+i
+        return content_str
